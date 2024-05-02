102820282fe39188d5b5e3bd50624aed5d29ced4
gye0n1 <1230rbdus@gmail.com>
2024-05-02 19:47:30 +0900

    🐛:수정중

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -14,6 +14,7 @@ function SignUp() {
       <S.title2>노나빌려</S.title2>
       <S.title3>입니다!</S.title3>
       <S.nickcheck>중복 확인</S.nickcheck>
+<<<<<<< Updated upstream
       <S.nickbox placeholder="닉네임" />
       <S.idbox placeholder="아이디를 입력해주세요" />
       <S.emailbox placeholder="이메일을 입력해주세요" />
@@ -23,6 +24,39 @@ function SignUp() {
       <S.passwordbox placeholder="비밀번호를 입력해주세요" />
       <S.passcheck placeholder="비밀번호를 다시 입력해주세요" />
       <S.phonenumbox placeholder="전화번호를 입력해주세요" />
+=======
+      <S.nickbox
+        placeholder="닉네임"
+        type="id"
+        id="id"
+        name="id"
+        onChange={handleSignupChange} />
+      <S.namebox
+        placeholder="이름을 입력해주세요"
+        type="name"
+        id="name"
+        name="name"
+        onChange={handleSignupChange}/>
+      <S.emailbox
+        placeholder="이메일을 입력해주세요"
+        type="email"
+        id="email"
+        name="email"
+        onChange={handleSignupChange} />
+      <S.emailcheck>중복 확인</S.emailcheck>
+      <S.passwordbox
+        placeholder="비밀번호를 입력해주세요"
+        type="password"
+        id="password"
+        name="password"
+        onChange={handleSignupChange}/>
+      <S.phonenumbox
+      placeholder="전화번호를 입력해주세요"
+      type="tell"
+        id="tell"
+        name="tell"
+        onChange={handleSignupChange}/>
+>>>>>>> Stashed changes
       <S.numcertifi>인증하기</S.numcertifi>
       <S.phonenumbox placeholder="전화번호를 입력해주세요" />
       <S.phonecertinum placeholder="인증 번호" />
