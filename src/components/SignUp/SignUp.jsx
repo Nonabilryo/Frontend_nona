17fb7abd81254e2908219aae88a0523316ac38c8
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-06-20 17:46:19 +0900

    🔥:가인님 코드로 교체

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,164 +1,3 @@
-// import React, { useCallback, useEffect, useState } from "react";
-// import * as S from "../../style/SignUp.style";
-// import cart from "../../assets/img/nonabilryo_cart.png";
-// import axios from "axios";
-// import CONFIG from "../../config/config.json";
-
-// function SignUp() {
-//   const [signUpData, setSignUpData] = useState({
-//     name: "",
-//     id: "",
-//     password: "",
-//     email: "",
-//     tell: "",
-//     adress: "",
-//     emailVerifyCode: "",
-//     tellVerifyCode: "",
-//   });
-
-//   const handleSignupChange = useCallback(
-//     (e) => {
-//       const { value, name } = e.target;
-//       setSignUpData((prev) => ({ ...prev, [name]: value }));
-//     },
-//     [setSignUpData]
-//   );
-
-//   const ServerConnect = async () => {
-//     const SignUpData = {
-//       name: signUpData.name,
-//       id: signUpData.id,
-//       password: signUpData.password,
-//       adress: null,
-//       emailVerifyCode: signUpData.emailVerifyCode,
-//       tellVerifyCode: signUpData.tellVerifyCode,
-//     };
-
-//     try {
-//       const { data } = await axios.post(
-//         `${CONFIG.SERVER}/sso/sign-up`,
-//         SignUpData
-//       );
-
-//       console.log("성공");
-//     } catch (e) {
-//       console.log("실패");
-//     }
-//   };
-
-//   useEffect(() => {
-//     ServerConnect();
-//   }, []);
-
-//   const EmailCheckHandler = async () => {
-//     const response = await axios.post(`${CONFIG.SERVER}/sso/verify/email`, {
-//       email: signUpData.email,
-//     });
-//     return response;
-//   };
-
-//   const EmailCheck = () => {
-//     EmailCheckHandler()
-//       .then((e) => console.log(e))
-//       .catch((e) => console.log(e));
-//   };
-
-//   const TellCheckHandler = async () => {
-//     const response = await axios.post(`${CONFIG.SERVER}/sso/verify/tell`, {
-//       tell: signUpData.tell,
-//     });
-//     return response;
-//   };
-
-//   const TellCheck = () => {
-//     TellCheckHandler()
-//       .then((e) => console.log(e))
-//       .catch((e) => console.log(e));
-//   };
-
-//   const NickCheckHandler = async () => {
-//     console.log(signUpData.name);
-//     const response = await axios.post(
-//       `${CONFIG.SERVER}/sso/verify/name`,
-//       {
-//         header: {
-//           "Content-Type": "application/json",
-//         },
-//         name: signUpData.name,
-//       },
-//       { withCredentials: true }
-//     );
-//     return response;
-//   };
-
-//   const NickCheck = () => {
-//     console.log(signUpData);
-//     NickCheckHandler()
-//       .then((e) => console.log(e))
-//       .catch((e) => console.log(e));
-//   };
-
-//   return (
-//     <>
-//       <S.back />
-//       <S.cart img src={cart} alt="cart" />
-//       <S.title1>환영합니다,</S.title1>
-//       <S.title2>노나빌려</S.title2>
-//       <S.title3>입니다!</S.title3>
-//       <S.nickcheck onClick={NickCheck}>중복 확인</S.nickcheck>
-
-//       <S.emailicertinum placeholder="인증 번호" />
-//       <S.emailpass>확인</S.emailpass>
-//       <S.passcheck placeholder="비밀번호를 다시 입력해주세요" />
-//       <S.phonenumbox placeholder="전화번호를 입력해주세요" />
-
-//       <S.nickbox
-//         placeholder="닉네임"
-//         type="text"
-//         id="name"
-//         name="name"
-//         onChange={handleSignupChange}
-//       />
-//       <S.idbox
-//         placeholder="아이디를 입력해주세요"
-//         type="text"
-//         id="id"
-//         name="id"
-//         onChange={handleSignupChange}
-//       />
-//       <S.emailbox
-//         placeholder="이메일을 입력해주세요"
-//         type="text"
-//         id="email"
-//         name="email"
-//         onChange={handleSignupChange}
-//       />
-//       <S.emailcheck onClick={EmailCheck}>중복 확인</S.emailcheck>
-//       <S.passwordbox
-//         placeholder="비밀번호를 입력해주세요"
-//         type="password"
-//         id="password"
-//         name="password"
-//         onChange={handleSignupChange}
-//       />
-//       <S.phonenumbox
-//         placeholder="전화번호를 입력해주세요"
-//         type="text"
-//         id="tell"
-//         name="tell"
-//         onChange={handleSignupChange}
-//       />
-//       <S.numcertifi onClick={TellCheck}>인증하기</S.numcertifi>
-//       <S.phonenumbox placeholder="전화번호를 입력해주세요" />
-//       <S.phonecertinum placeholder="인증 번호" />
-//       <S.phonepass>확인</S.phonepass>
-
-//       <S.signup>회원가입</S.signup>
-//     </>
-//   );
-// }
-// export default SignUp;
-
 import React, { useCallback, useEffect, useState } from "react";
 import * as S from "../../style/SignUp.style";
 import cart from "../../assets/img/nonabilryo_cart.png";
@@ -200,7 +39,7 @@ function SignUp() {
 
   const EmailCheckHandler = async () => {
     const response = await axios.post(
-      `${CONFIG.SERVER}/sso/verify/email`,
+      `/sso/verify/email`,
       {
         headers: {
           'Content-Type': 'application/json'
@@ -220,7 +59,7 @@ function SignUp() {
 
   const TellCheckHandler = async () => {
     const response = await axios.post(
-      `${CONFIG.SERVER}/sso/verify/tell`,
+      `/sso/verify/tell`,
       {
         headers: {
           'Content-Type': 'application/json'
@@ -241,7 +80,7 @@ function SignUp() {
   const NickCheckHandler = async () => {
     console.log(signUpData.name)
     const response = await axios.post(
-      `${CONFIG.SERVER}/sso/verify/name`,
+      `/sso/verify/name`,
       {
         headers: {
           'Content-Type': 'application/json'
