6fc573e354c0e60feb9d89bad83922783c82f6d7
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2024-07-16 16:16:31 +0900

    🐛::  상세 수정

--- src/style/PostArticle.js
+++ src/style/PostArticle.js
@@ -6,7 +6,6 @@ export const title = styled.div`
   margin-top: 9.3vw;
   margin-left: 16vw;
   position: absolute;
-
 `;
 
 export const tiInput = styled.input`
@@ -44,20 +43,20 @@ export const priInput = styled.input`
 export const desc = styled.div`
   font-family: pretendard-semibold;
   font-size: 1.5vw;
-  margin-top: 17vw;
+  margin-top: 21vw;
   margin-left: 16vw;
   position: absolute;
 `;
 
 export const descInput = styled.input`
   width: 50vw;
-  height: 25vw;
+  height: 15vw;
   margin-left: 28vw;
   border-color: #3bd15f;
   border-width: 0.2vw;
   padding-left: 1vw;
   position: absolute;
-  margin-top: 17vw;
+  margin-top: 21vw;
   border-radius: 0.7vw;
 `;
 
@@ -69,24 +68,42 @@ export const img = styled.div`
   position: absolute;
 `;
 
-
 export const imgInput = styled.input`
-    margin-left: 28vw;
-    margin-top: 45vw;
-    position: absolute;
+  margin-left: 28vw;
+  margin-top: 45vw;
+  position: absolute;
 `;
 
 export const sub = styled.button`
+  width: 10vw;
+  height: 3.3vw;
+  margin-left: 70vw;
+  margin-top: 50vw;
+  position: absolute;
+  border: none;
+  background-color: #3bd15f;
+  color: white;
+  font-size: 2vw;
+  font-family: pretendard-semibold;
+  border-radius: 1vw;
+`;
 
-    width: 10vw;
-    height: 3.3vw;
-    margin-left: 70vw;
-    margin-top: 50vw;
-    position: absolute;
-    border: none;
-    background-color: #3BD15F;
-    color: white;
-    font-size: 2vw;
-    font-family: pretendard-semibold;
-    border-radius: 1vw;
+export const cate = styled.div`
+  font-family: pretendard-semibold;
+  font-size: 1.5vw;
+  margin-top: 17vw;
+  margin-left: 16vw;
+  position: absolute;
+`;
+
+export const cateInput = styled.input`
+  width: 50vw;
+  height: 2vw;
+  margin-left: 28vw;
+  border-color: #3bd15f;
+  border-width: 0.2vw;
+  padding-left: 1vw;
+  position: absolute;
+  margin-top: 17vw;
+  border-radius: 0.7vw;
 `;
