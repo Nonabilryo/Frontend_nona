9078470aff6ad4792518ac05a051bbca523d97cf
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-09-12 20:17:28 +0900

    rename : create chattingPage file

--- src/components/Chat/ChattingPage.jsx
+++ src/components/Chat/ChattingPage.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import * as S from '../../style/Chatting.style'
+import React from "react";
+import * as S from "../../style/Chatting.style";
+import userImg from "../../assets/img/user.svg";
+import searchImg from "../../assets/img/searchGray.svg";
 
 const ChattingPage = () => {
   return (
-    <div>ChattingPage</div>
-  )
-}
+    <>
+      <S.all>
+        <S.frontNlast>
+          <S.frontTop>
+            <S.searchArea>
+              <S.search 
+              type="text"
+              placeholder="검색"></S.search>
+              <S.searchImg src={searchImg} />
+            </S.searchArea>
+          </S.frontTop>
+        </S.frontNlast>
+        <S.middle>
+          <S.middleArea>
+            <div>
+              <S.userImg src={userImg} />
+              <div>
+                <div>가인님</div>
+                <div>활동 중</div>
+              </div>
+            </div>
+          </S.middleArea>
+        </S.middle>
+        <S.frontNlast></S.frontNlast>
+      </S.all>
+    </>
+  );
+};
 
 export default ChattingPage;
--- src/style/Chatting.style.js
+++ src/style/Chatting.style.js
@@ -1,5 +1,63 @@
 import styled from "styled-components";
 
-export const All = styled.div`
+export const all = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 
+export const middle = styled.div`
+width: 100%;
+border-left: 1.5px solid #E4E4E4;
+border-right: 1.5px solid #E4E4E4;
+  background-color: white;
+  
+`;
+
+export const frontNlast = styled.div`
+background-color: white;
+width: 50vw;
+`;
+
+export const frontTop = styled.div`
+width: 100%;
+height: 13vh;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+export const searchArea = styled.div`
+display: flex;
+align-items: center;
+background-color: #F1F0F0;
+border-radius: 5px;
+padding-right: 0.5vw;
+`;
+
+export const search = styled.input`
+width: 16vw;
+height: 4.5vh;
+border: none;
+outline: none;
+background-color: #F1F0F0;
+border-radius: 5px;
+padding-left: 1vw;
+padding-right: 1vw;
+color : #8F8F8F;
+`;
+
+export const searchImg = styled.img`
+width: 1.5vw;
+`;
+
+export const middleArea = styled.div`
+width: 100%;
+height: 10vh;
+background-color: red;
+`;
+
+export const userImg = styled.img`
+width: 4vw;
 `;
