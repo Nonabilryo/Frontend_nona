eafa4ca0da085d9d8c821a46d850213c24f37411
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2024-10-22 23:51:15 +0900

    🚑

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+import CONFIG from "../../config/config.json";
+
 import * as M from "../../style/MyPage";
 import "../../style/MyPage";
 import twoline from "../../assets/img/twoline.png";
@@ -8,9 +11,44 @@ import userpro from "../../assets/img/userprofile.png";
 function MyPage() {
   const navigate = useNavigate();
   const location = useLocation();
+  const accessToken = localStorage.getItem("accessToken");
 
-  
+  const [userData, setUserData] = useState({
+    idx: "",
+    name: "",
+    imageUrl: "",
+    description: "",
+    signed: "",
+  });
 
+  const fetchUserData = async () => {
+    try {
+      const response = await axios.get(
+        `${CONFIG.SERVER}/user/${location.pathname.split("/")[2]}`,
+        { withCredentials: true }
+      );
+
+      setUserData({
+        idx: response.data.data.idx,
+        name: response.data.data.name,
+        imageUrl: response.data.data.imageUrl,
+        description: response.data.data.description,
+        signed: response.data.data.signed,
+      });
+
+      const userDataResponse = await axios.get(
+        `${CONFIG.SERVER}/user/${response.data.data.user}`,
+        { withCredentials: true }
+      );
+      setUserData(userDataResponse.data.data);
+    } catch (error) {
+      if (error.response && error.response.status === 302) {
+        window.location.href = error.response.headers.location;
+      } else {
+        console.error("데이터를 불러오는 도중 오류 발생:", error);
+      }
+    }
+  };
 
   return (
     <>
